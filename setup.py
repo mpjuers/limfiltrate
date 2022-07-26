d8984c186435de192d4254dac74fa3e735d55bf4
Mark Juers <mpjuers@indiana.edu>
2022-07-26 11:45:07 -0400

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(name='LimFiltrate',
+      version='0.1',
+      description='A PCA-based visual tool for optimizing flow cytometry filters.',
+      author='Neko Juers',
+      author_email='mpjuers.professional@gmail.com',
+      url='',
+      packages=['limfiltrate'],
+     )
