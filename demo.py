d336d50e5cbd91284317d6f28cbe01a749512aef
Mark Juers <mpjuers@indiana.edu>
2022-07-28 13:40:46 -0400

    Change demo.py to include sorting on long particles.

--- demo.py
+++ demo.py
@@ -9,22 +9,36 @@ from sklearn.decomposition import PCA
 pca = PCA()
 scaler = MinMaxScaler()
 
-df = pd.read_csv("Data/2022-07-26_test4_with-classification.csv")
+df = pd.read_csv("Data/2022-07-28_autoimage-test_2.csv")
+df.columns = (
+    df.columns.str.replace(" ", "_")
+    .str.replace("(", "")
+    .str.replace(")", "")
+    .str.replace("/", "-")
+    .str.replace(".", "")
+    .str.lower()
+)
+
+df["longskinnies"] = df.aspect_ratio.map(
+    lambda x: "longskinny" if x < 0.25 else "round"
+)
 
 df_scaled = scaler.fit_transform(df.select_dtypes(np.number))
+
 pca_out = pca.fit_transform(df_scaled)
 labels = {
     str(i): f"PC {i+1} ({var:.1f}%)"
     for i, var in enumerate(pca.explained_variance_ratio_ * 100)
 }
 
-n_pcs = 5
+n_pcs = range(2, 7)
 
 fig = px.scatter_matrix(
     pca_out,
     labels=labels,
-    dimensions=range(n_pcs),
-    color=df["class"],
+    dimensions=n_pcs,
+    color=df["longskinnies"],
+    opacity=0.15,
 )
 
 fig.update_traces(diagonal_visible=False)
