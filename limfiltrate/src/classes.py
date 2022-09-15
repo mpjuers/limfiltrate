feae0053fdd1db4216d8fabd4881d710e95eb259
Mark Juers <mpjuers@indiana.edu>
2022-09-15 12:13:37 -0400

    Began from scratch, implementing unit testing along the way.

--- limfiltrate/src/classes.py
+++ limfiltrate/src/classes.py
@@ -4,9 +4,6 @@ import pandas as pd
 
 class Analysis:
     def __init__(self, csv):
-        self.csv = csv
+        self.data = pd.read_csv(csv)
         return None 
 
-    def read_data(self):
-        pd.read_csv(self.csv)
-        return True
